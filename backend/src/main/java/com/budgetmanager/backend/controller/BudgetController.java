51a268112f3d737250f5af409fcce1bed7f95b33
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-05-14 14:50:46 +0100

    ajouter budget

--- backend/src/main/java/com/budgetmanager/backend/controller/BudgetController.java
+++ backend/src/main/java/com/budgetmanager/backend/controller/BudgetController.java
@@ -1,4 +1,22 @@
 package com.budgetmanager.backend.controller;
 
+import com.budgetmanager.backend.Models.Budget;
+import com.budgetmanager.backend.Services.BudgetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller("/api/v1/budgets")
 public class BudgetController {
+
+    final BudgetService budgetService;
+
+    @Autowired
+    public BudgetController(BudgetService budgetService) {
+        this.budgetService = budgetService;
+    }
+
+    public String addBudget(Budget budget) {
+        budgetService.addBudget(budget);
+        return ("your budget is added successfully");
+    }
 }
