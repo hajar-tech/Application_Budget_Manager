573c55d43a3d20b2be147ea78c236c39ef5bae52
imaneboho2159 <imaboh21@gmail.com>
2025-05-14 11:49:42 +0100

    add the function of the transaction in service and controlleur

--- backend/src/main/java/com/budgetmanager/backend/controller/TransactionController.java
+++ backend/src/main/java/com/budgetmanager/backend/controller/TransactionController.java
@@ -1,4 +1,21 @@
 package com.budgetmanager.backend.controller;
 
+import com.budgetmanager.backend.Models.Transaction;
+import com.budgetmanager.backend.Services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class TransactionController {
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public Transaction AjouterTransaction(@RequestBody Transaction transaction){
+        return transactionService.AjouterTransaction(transaction);
+    }
 }
