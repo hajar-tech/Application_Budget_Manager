45a1e1632a35354216a6267437afda4922b09657
imaneboho2159 <imaboh21@gmail.com>
2025-05-14 15:08:27 +0100

    add the fnction of add and display of category

--- backend/src/main/java/com/budgetmanager/backend/Services/CategoryService.java
+++ backend/src/main/java/com/budgetmanager/backend/Services/CategoryService.java
@@ -1,7 +1,23 @@
 package com.budgetmanager.backend.Services;
 
 
-public class CategoryService {
+import com.budgetmanager.backend.Models.Category;
+import com.budgetmanager.backend.Repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
 
 }
--- backend/src/main/java/com/budgetmanager/backend/controller/CategorieController.java
+++ backend/src/main/java/com/budgetmanager/backend/controller/CategorieController.java
@@ -1,7 +1,28 @@
 package com.budgetmanager.backend.controller;
 
 
+import com.budgetmanager.backend.Models.Category;
+import com.budgetmanager.backend.Services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/category")
 public class CategorieController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        return ResponseEntity.ok(categoryService.createCategory(category));
+    }
 
 }
