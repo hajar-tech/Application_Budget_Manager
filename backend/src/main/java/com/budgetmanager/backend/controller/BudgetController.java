a9b4d26809f6185ea449f8895a9362bf1379fdb2
hajar zerhouni <hajar.zerhounipr@gmail.com>
2025-05-15 11:47:47 +0100

    création dz la fonction d'affichage d'un budget avec les infos de la category qu'il l'associe

--- backend/src/main/java/com/budgetmanager/backend/controller/BudgetController.java
+++ backend/src/main/java/com/budgetmanager/backend/controller/BudgetController.java
@@ -1,30 +1,52 @@
 package com.budgetmanager.backend.controller;
 
 import com.budgetmanager.backend.Dto.BudgetDto;
+import com.budgetmanager.backend.Dto.BudgetResponseDto;
+import com.budgetmanager.backend.Dto.CategoryDto;
 import com.budgetmanager.backend.Models.Budget;
+import com.budgetmanager.backend.Models.Category;
+import com.budgetmanager.backend.Repositories.BudgetRepository;
 import com.budgetmanager.backend.Services.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api/v1/budgets")
 @RestController
 public class BudgetController {
 
+    final BudgetRepository budgetRepository;
     final BudgetService budgetService;
 
     @Autowired
-    public BudgetController( @RequestBody BudgetService budgetService) {
+    public BudgetController( @RequestBody BudgetService budgetService , BudgetRepository budgetRepository) {
         this.budgetService = budgetService;
+        this.budgetRepository = budgetRepository;
     }
 
     @PostMapping("/AddBudget")
     public ResponseEntity<Budget> createBudget(@RequestBody BudgetDto budgetDto) {
         Budget createdBudget = budgetService.addBudget(budgetDto);
         return ResponseEntity.ok(createdBudget);
+    }
+
+
+    @GetMapping("/GetBudgets")
+    public List<BudgetResponseDto> getAllBudgets() {
+        return budgetRepository.findAll().stream()
+                .map(budget -> {
+                    Category cat = budget.getCategory();
+                    return new BudgetResponseDto(
+                            budget.getLimitBudget(),
+                            budget.getStartDate(),
+                            budget.getEndDate(),
+                            new CategoryDto(cat.getName(), cat.getType())
+                    );
+                })
+                .collect(Collectors.toList());
     }
 }
