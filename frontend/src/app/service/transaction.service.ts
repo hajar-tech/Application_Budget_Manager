afc27bfde664a763691f2831c310fd45cf30f8de
imaneboho2159 <imaboh21@gmail.com>
2025-05-17 12:56:50 +0200

    create the srvice of transaction

--- frontend/src/app/service/transaction.service.ts
+++ frontend/src/app/service/transaction.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+class Transaction {
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionService {
+  private api = 'http://localhost/8080/api/transactions';
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  getTransactions():Observable<Transaction[]>{
+    return this.http.get<Transaction[]>(this.api);
+  }
+  addTransaction(transaction:Transaction):Observable<Transaction>{
+    return this.http.post(this.api, transaction);
+  }
 }
