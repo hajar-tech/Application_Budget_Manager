8ab1e37b2e08d32df4d80bf25382a9e8f1f6002a
imaneboho2159 <imaboh21@gmail.com>
2025-05-17 12:59:23 +0200

    create the srvice of category

--- frontend/src/app/service/category.service.ts
+++ frontend/src/app/service/category.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+class Category {
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
-}
+  private api = 'http://localhost/8080/api/transactions';
+
+  constructor( private http: HttpClient ) { }
+
+  getTransactions():Observable<Category[]>{
+    return this.http.get<Category[]>(this.api);
+  }
+  addTransaction(category: Category):Observable<Category>{
+    return this.http.post(this.api,category);
+  }}
